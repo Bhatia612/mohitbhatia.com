bb28b69c26d9525b9e532185b38ce9c835319cf8
MOHIT <mohitbhatia612@gmail.com>
2025-04-26 22:26:07 -0500

    . . .

--- src/components/IntroPage.jsx
+++ src/components/IntroPage.jsx
@@ -26,19 +26,18 @@ function IntroPage() {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
       <div className="loader flex gap-2 font-exo2 text-4xl font-extrabold text-gray-900">
-        <div className="letter" data-letter = "M">M</div>
-        <div className="letter" data-letter = "o">o</div>
-        <div className="letter" data-letter = "h">h</div>
-        <div className="letter" data-letter = "i">i</div>
-        <div className="letter" data-letter = "t">t</div>
-        <div className="letter" data-letter = " "> </div>
-        <div className="letter" data-letter = "B">B</div>
-        <div className="letter" data-letter = "h">h</div>
-        <div className="letter" data-letter = "a">a</div>
-        <div className="letter" data-letter = "t">t</div>
-        <div className="letter" data-letter = "i">i</div>
-        <div className="letter" data-letter = "a">a</div>
-
+        <div className="letter" data-letter="M">M</div>
+        <div className="letter" data-letter="o">o</div>
+        <div className="letter" data-letter="h">h</div>
+        <div className="letter" data-letter="i">i</div>
+        <div className="letter" data-letter="t">t</div>
+        <div className="letter" data-letter=" "> </div>
+        <div className="letter" data-letter="B">B</div>
+        <div className="letter" data-letter="h">h</div>
+        <div className="letter" data-letter="a">a</div>
+        <div className="letter" data-letter="t">t</div>
+        <div className="letter" data-letter="i">i</div>
+        <div className="letter" data-letter="a">a</div>
       </div>
     </div>
   );
